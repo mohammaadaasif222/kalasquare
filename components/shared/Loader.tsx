be50030cba4984900d3222946ca6c0fbb57f51a5
mohamamd <kasifsaif784@gmail.com>
2025-10-21 10:02:17 +0530

    update loader

--- components/shared/Loader.tsx
+++ components/shared/Loader.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Loader() {
-    return (
-        <div className='loader'>
-
-        </div>
-    )
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <div className='loader'></div>
+    </div>
+  )
 }
